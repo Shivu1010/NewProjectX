fc7c7882fab79cb3af01b556fb08efd66cfa6db9
Shivanand_Amminabhavi <shivuplacement@gmail.com>
2024-11-16 13:30:03 +0530

    upload files

---
+++ account/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import profile, update_profile
+
+urlpatterns = [
+    path('profile/', profile, name='profile'),
+    path('update/', update_profile, name='update-profile')
+   
+]
